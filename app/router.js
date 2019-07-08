5fe0ad873364f3ef9b127d2469b6ddaf91daeba5
kevinxft <kevinxft@gmail.com>
2019-07-08 16:46:48 +0800

    fix icon

--- app/router.js
+++ app/router.js
@@ -3,5 +3,6 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.index.index.index)
   router.get('/contact', controller.index.index.index)
+  router.get('/whoWeAre', controller.index.index.index)
   router.get('/letter', controller.index.index.index);
 };
