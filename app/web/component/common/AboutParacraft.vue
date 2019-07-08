4301cbea32cc21bf8466743eb685badc57a35eda
DDXY1230 <2502752288@qq.com>
2019-07-08 17:27:32 +0800

    add home page

---
+++ app/web/component/common/AboutParacraft.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="about-paracraft">
+    <div class="about-paracraft-title">
+      <span class="about-paracraft-title-icon"><img src="@/asset/images/box2.png" alt=""></span>
+      <span class="about-paracraft-title-text">About Paracraft</span>
+      <span class="about-paracraft-title-icon"><img src="@/asset/images/box2.png" alt=""></span>
+    </div>
+    <div class="about-paracraft-main">
+      <div class="about-paracraft-main-left">
+        <p>
+          Paracraft is a 3D animation and programming tool for people aging above 7. You can use it to create animation movies, games or any 3D works. Kids love playing and creating works with paracraft, and share with their friends.
+        </p>
+        <p>
+          We share the same believes as Scratch in the importance of programming as well as how best to learn. Indeed, you can think of us as an upgrade of Scratch. We made it 3D, and thus it is more fun and attractive to children. It is a great environment for children to start learning of programming. And you can progress smoothly to a much deeper level and getting really serious with programming if you really fall in love with programming. If you are not ready to get serious with programming, you still can learn basics of programming, know what it is about, and you probably will find yourself passionately building great 3D scenes and moves instead! We found kids of all interests and all ages find their passion in paracraft and get themselves busy building things and learning things. So buckle up, and be ready to take a great adventure! Life will unfold before you like a beautiful flower.
+        </p>
+      </div>
+      <div class="about-paracraft-main-right">
+        <div class="about-paracraft-main-right-video">
+          <video class="about-paracraft-main-right-video-content" controls :src="currenPlayVideoUrl"></video>
+        </div>
+        <div class="about-paracraft-main-right-buttons">
+          <span :class="['about-paracraft-main-right-buttons-button',{selected: 0 === selectedVideo}]" @click="playVideo(0)"><img class="play-icon" :src="0 === selectedVideo ? selectedPlayIcon_0 : selectedPlayIcon_1" alt="">What is Paracraft</span>
+          <span :class="['about-paracraft-main-right-buttons-button',{selected: 1 === selectedVideo}]" @click="playVideo(1)"><img class="play-icon" :src="1 === selectedVideo ? selectedPlayIcon_0 : selectedPlayIcon_1" alt="">Basic Tutorial</span>
+        </div>
+      </div>
+    </div>
+    <div class="about-paracraft-letters">
+      <a class="about-paracraft-letters-link" href="https://keepwork.com/leonliu/asde/how_to_learn_programming" target="_blank"><img src="@/asset/images/letter.png" alt="">Founder Letter</a>
+      <a class="about-paracraft-letters-link" href="https://keepwork.com/official/paracraft/to-parents" target="_blank"><img src="@/asset/images/letter.png" alt="">To Parents</a>
+      <a class="about-paracraft-letters-link" href="https://keepwork.com/official/paracraft/to-educators" target="_blank"><img src="@/asset/images/letter.png" alt="">To Educators</a>
+      <a class="about-paracraft-letters-link" href="https://keepwork.com/official/paracraft/to-developers" target="_blank"><img src="@/asset/images/letter.png" alt="">To Developers</a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'AboutParacraft',
+  data() {
+    return {
+      selectedVideo: 0,
+      selectedPlayIcon_0: require('@/asset/images/play_white.png'),
+      selectedPlayIcon_1: require('@/asset/images/play_black.png')
+    }
+  },
+  computed: {
+    currenPlayVideoUrl() {
+      return this.selectedVideo == 0
+        ? 'https://www.youtube.com/watch?v=KfpqXMLNtLY'
+        : 'https://www.youtube.com/watch?v=uUP4ZOp9Vgo'
+    }
+  },
+  methods: {
+    playVideo(index) {
+      this.selectedVideo = index
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.about-paracraft {
+  max-width: 1200px;
+  margin: 60px auto;
+  &-title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &-icon {
+      margin: 0 20px;
+    }
+    &-text {
+      font-size: 26px;
+      // font-weight: bold;
+      font-family: 'OpenSans-Semibold';
+    }
+  }
+  &-main {
+    display: flex;
+    &-left {
+      width: 558px;
+      font-size: 16px;
+      color: #666;
+      line-height: 30px;
+      letter-spacing: 0;
+    }
+    &-right {
+      flex: 1;
+      &-video {
+        max-width: 590px;
+        background: rgb(241, 241, 241);
+        box-sizing: border-box;
+        padding: 16px;
+        border-radius: 8px;
+        margin: 20px auto;
+        &-content {
+          width: 100%;
+        }
+      }
+      &-buttons {
+        display: flex;
+        justify-content: center;
+        &-button {
+          margin: 30px;
+          padding: 12px 50px;
+          cursor: pointer;
+          border-radius: 30px;
+          color: #333;
+          font-weight: bold;
+          background: #ccc;
+          display: flex;
+          align-items: center;
+          .play-icon {
+            margin-right: 12px;
+          }
+          &.selected {
+            color: white;
+            background: #438cff;
+          }
+        }
+      }
+    }
+  }
+  &-letters {
+    display: flex;
+    &-link {
+      text-align: center;
+      flex: 1;
+      color: #409eff;
+      font-weight: bold;
+      img {
+        margin-right: 10px;
+      }
+      & + .about-paracraft-letters-link {
+        border-left: 1px solid #ccc;
+      }
+    }
+  }
+}
+</style>
+
+
---
+++ app/web/component/common/UserWorks.vue
@@ -0,0 +1,221 @@
+<template>
+  <div class="user-works">
+    <div class="user-works-title">
+      <span class="user-works-title-icon"><img src="@/asset/images/box2.png" alt=""></span>
+      <span class="user-works-title-text">Recommended User works</span>
+      <span class="user-works-title-icon"><img src="@/asset/images/box2.png" alt=""></span>
+    </div>
+    <div class="user-works-cabinet">
+      <div class="user-works-cabinet-box" @click="playCurrentVideo(index)" v-for="(item,index) in userWorksData" :key="index">
+        <div class="user-works-cabinet-box-video">
+          <img class="user-works-cabinet-box-video-cover" :src="item.poster" alt="">
+          <img class="user-works-cabinet-box-video-play" src="@/asset/images/play.png" alt="">
+        </div>
+        <p class="user-works-cabinet-box-name">{{item.name}}</p>
+      </div>
+    </div>
+    <img class="user-works-bgpic-1" src="@/asset/images/c.png" alt="">
+    <img class="user-works-bgpic-2" src="@/asset/images/c.png" alt="">
+    <img class="user-works-bgpic-3" src="@/asset/images/c.png" alt="">
+    <div class="user-works-bottom">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'UserWorks',
+  data() {
+    return {
+      userWorksData: [
+        {
+          poster: require('@/asset/images/works/1.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=OInkv0lQYNQ',
+          name: 'I Am The Director'
+        },
+        {
+          poster: require('@/asset/images/works/2.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=suNY6hPINw8',
+          name: 'Heart And Hands'
+        },
+        {
+          poster: require('@/asset/images/works/3.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=Fu0dELWhssY',
+          name: 'The Boy and the Apple Tree'
+        },
+        {
+          poster: require('@/asset/images/works/4.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=URZ-RB9did8',
+          name: 'Round Story'
+        },
+        {
+          poster: require('@/asset/images/works/5.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=Lovl1obZdo4',
+          name: 'Round Story 2'
+        },
+        {
+          poster: require('@/asset/images/works/6.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=TZX2wUqTXDI',
+          name: 'What Do You Do With An Idea?'
+        },
+        {
+          poster: require('@/asset/images/works/7.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=k0YP7kUC7G4',
+          name: 'I Had A Black Dog'
+        },
+        {
+          poster: require('@/asset/images/works/8.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=zzNWV8ECu-Q',
+          name: 'Liyuan Primary School'
+        },
+        {
+          poster: require('@/asset/images/works/9.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=GJkQPYPYCjA',
+          name: 'A day for the hungry'
+        },
+        {
+          poster: require('@/asset/images/works/10.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=OklGeZmn7O8',
+          name: 'To Mars'
+        },
+        {
+          poster: require('@/asset/images/works/11.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=GVa_nGgMbc4',
+          name: 'Mars Exploration'
+        },
+        {
+          poster: require('@/asset/images/works/12.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=itkrGkOHonE',
+          name: 'The Great Fireman'
+        },
+        {
+          poster: require('@/asset/images/works/13.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=dOny21WZHA0',
+          name: 'The Great Inventor'
+        },
+        {
+          poster: require('@/asset/images/works/14.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=MPhAx9RniLg',
+          name: 'Mini Thing'
+        },
+        {
+          poster: require('@/asset/images/works/15.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=FPURHLPj-J8',
+          name: 'Black Dog'
+        },
+        {
+          poster: require('@/asset/images/works/16.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=0cCBnkOj_WU',
+          name: 'The Clown'
+        },
+        {
+          poster: require('@/asset/images/works/17.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=qlHiSdFe2T8',
+          name: 'Eternal Snowman'
+        },
+        {
+          poster: require('@/asset/images/works/18.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=pauv07XvFHk',
+          name: 'Shine Your Way'
+        },
+        {
+          poster: require('@/asset/images/works/19.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=56H0z396LPM',
+          name: 'Beautiful Mind'
+        },
+        {
+          poster: require('@/asset/images/works/20.png'),
+          videoUrl: 'https://www.youtube.com/watch?v=QwY4xgJcqKs',
+          name: 'Unfinished'
+        }
+      ]
+    }
+  },
+  methods: {
+    playCurrentVideo(index) {
+      window.open(this.userWorksData[index].videoUrl)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.user-works {
+  background: #4793ff;
+  padding: 72px 0 0;
+  position: relative;
+  &-bgpic-1 {
+    position: absolute;
+    top: 300px;
+    left: 100px;
+  }
+  &-bgpic-2 {
+    position: absolute;
+    right: 100px;
+    bottom: 300px;
+  }
+  &-bgpic-3 {
+    position: absolute;
+    bottom: 100px;
+    left: -100px;
+  }
+  &-title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &-icon {
+      margin: 0 20px;
+    }
+    &-text {
+      font-size: 26px;
+      color: #fff;
+      font-family: 'OpenSans-Semibold';
+    }
+  }
+  &-cabinet {
+    position: relative;
+    z-index: 999;
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    &-box {
+      width: 285px;
+      padding: 16px 16px 5px;
+      background: #fff;
+      margin: 7px 7px;
+      border-radius: 8px;
+      cursor: pointer;
+      &:hover {
+        .user-works-cabinet-box-video-play {
+          opacity: 1;
+        }
+      }
+      &-video {
+        position: relative;
+        &-cover {
+          width: 100%;
+          height: 143px;
+          object-fit: cover;
+        }
+        &-play {
+          position: absolute;
+          left: 50%;
+          top: 50%;
+          transform: translateX(-50%) translateY(-50%);
+          opacity: 0;
+        }
+      }
+      &-name {
+        text-align: center;
+        margin: 10px 0 0 0;
+      }
+    }
+  }
+  &-bottom {
+    height: 71px;
+    margin-top: 72px;
+    background: url(../../asset/images/semicircle.png);
+  }
+}
+</style>
+
